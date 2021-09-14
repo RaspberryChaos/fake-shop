1dfe911b71aca2fd1dc3f685f927828c144e7088
Ailie McCorkindale <ailiem134@hotmail.com>
2021-09-15 00:39:30 +0200

    routes set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Header from './containers/Header';
 
 function App() {
   return (
     <div className="App">
+    <Router>
       <Header />
+      <Switch>
+        <Route path="/" exact component={ProductListing} />
+        <Route path="/product/:productId" exact component={ProductDetail} />
+        <Route>404 Not Found!</Route>
+      </Switch>
+
+    </Router>
     </div>
   );
 }
